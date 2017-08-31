c844e6beb4bf50de9c479d09d07eda5878fb6f5d
Julien Danjou <julien@danjou.info>
2017-08-31 10:02:40 +0200

    Use flake8-import-order
    Signed-off-by: Julien Danjou <julien@danjou.info>

--- daiquiri/tests/test_output.py
+++ daiquiri/tests/test_output.py
@@ -9,8 +9,8 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
-from datetime import timedelta
 import syslog
+from datetime import timedelta
 
 import testtools
 
